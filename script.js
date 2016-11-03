adf637a93597cbd12cd0e9610ab01cfe18bc59d3
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-11-03 15:04:25 -0600

    vanilla js light up red yellow green

--- script.js
+++ script.js
@@ -11,36 +11,40 @@
 
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   var buttons = document.getElementsByName('button');
-//
-//   var stop = document.getElementById('stopButton');
-//
-//   stop.addEventListener("click", function () {
-//     console.log(buttons.stoplight);
-//     // console.log(buttons.stopLight[0].style.backgroundColor = "red");
-//   });
-//   // //
-//   // var slow = document.getElementById('slowButton');
-//   // slow.addEventListener("click", function () {
-//   //   buttons.slowLight[0].style.backgroundColor = "yellow";
-//   // });
-//   //
-//   // var go = document.getElementById('goButton');
-//   // go.addEventListener("click", function () {
-//   //
-//   //     buttons.goLight[0].style.backgroundColor = "green";
-//   //   })
-// });
-$(document).ready(function () {
-  $('#stopButton').click(function(t) {
-    $('#stopLight.bulb').css("background-color", "red");
-  })
-  $('#slowButton').click(function() {
-    $('#slowLight.bulb').css("background-color", "yellow");
-  })
-  $('#goButton').click(function() {
-    $('#goLight.bulb').css("background-color", "green");
+document.addEventListener('DOMContentLoaded', function() {
+  // var buttons = document.getElementsByName('button');
+  var stop = document.getElementById('stopButton');
 
-  })
-})
+  stop.addEventListener('click', function () {
+    var stoplight = document.getElementById('stopLight');
+    stoplight.style.backgroundColor = 'red';
+  });
+
+  var slow = document.getElementById('slowButton');
+  slow.addEventListener('click', function () {
+    var slowLight = document.getElementById('slowLight');
+    slowLight.style.backgroundColor = 'yellow';
+  });
+
+  var go = document.getElementById('goButton');
+  go.addEventListener('click', function () {
+    var goLight = document.getElementById('goLight');
+    goLight.style.backgroundColor = 'green';
+  });
+});
+
+
+
+////jquery
+// $(document).ready(function () {
+//   $('#stopButton').click(function(t) {
+//     $('#stopLight.bulb').css("background-color", "red");
+//   })
+//   $('#slowButton').click(function() {
+//     $('#slowLight.bulb').css("background-color", "yellow");
+//   })
+//   $('#goButton').click(function() {
+//     $('#goLight.bulb').css("background-color", "green");
+//
+//   })
+// })
