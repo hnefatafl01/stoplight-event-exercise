f73ed7d1b0d693b8e5f7e418dbc27f67e9c0c4e3
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-11-02 23:25:24 -0600

    used JQuery to turn lights on with click

--- script.js
+++ script.js
@@ -8,3 +8,39 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   var buttons = document.getElementsByName('button');
+//
+//   var stop = document.getElementById('stopButton');
+//
+//   stop.addEventListener("click", function () {
+//     console.log(buttons.stoplight);
+//     // console.log(buttons.stopLight[0].style.backgroundColor = "red");
+//   });
+//   // //
+//   // var slow = document.getElementById('slowButton');
+//   // slow.addEventListener("click", function () {
+//   //   buttons.slowLight[0].style.backgroundColor = "yellow";
+//   // });
+//   //
+//   // var go = document.getElementById('goButton');
+//   // go.addEventListener("click", function () {
+//   //
+//   //     buttons.goLight[0].style.backgroundColor = "green";
+//   //   })
+// });
+$(document).ready(function () {
+  $('#stopButton').click(function(t) {
+    $('#stopLight.bulb').css("background-color", "red");
+  })
+  $('#slowButton').click(function() {
+    $('#slowLight.bulb').css("background-color", "yellow");
+  })
+  $('#goButton').click(function() {
+    $('#goLight.bulb').css("background-color", "green");
+
+  })
+})
