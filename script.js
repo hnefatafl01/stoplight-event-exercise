0fc0f6c9616e6319d7cb4298fd26f0602c45ba93
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-11-22 09:29:03 -0700

    started refactor

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var stop = document.getElementById('stopButton');
-    stop.addEventListener('click', function () {
-        var stopLight = document.getElementById('stopLight');
-        var redLight = stopLight.style.backgroundColor;
-        if (redLight !== 'red') {
-            stopLight.style.backgroundColor = 'red';
-            slowLight.style.backgroundColor = '#111';
-            goLight.style.backgroundColor = '#111';
-        } else {
-            stopLight.style.backgroundColor = '#111';
-        }
-    });
-    var slow = document.getElementById('slowButton');
-    slow.addEventListener('click', function () {
+
+    var stopButton = document.getElementById('stopButton');
+
+
+
+        stopButton.addEventListener('click', function () {
+            var stopLight = document.getElementById('stopLight');
+            var redLight = stopLight.style.backgroundColor;
+            function stop() {
+                if (redLight !== 'red') {
+                    stopLight.style.backgroundColor = 'red';
+                    slowLight.style.backgroundColor = '#111';
+                    goLight.style.backgroundColor = '#111';
+                } else {
+                    stopLight.style.backgroundColor = '#111';
+                }
+            };
+            stop();
+        });
+
+
+
+
+
+
+
+
+    var slowButton = document.getElementById('slowButton');
+    slowButton.addEventListener('click', function () {
         var slowLight = document.getElementById('slowLight');
         var light = slowLight.style.backgroundColor;
-        if (light !== 'yellow') {
-            slowLight.style.backgroundColor = 'yellow';
-            stopLight.style.backgroundColor = '#111';
-            goLight.style.backgroundColor = '#111';
-        } else {
-            slowLight.style.backgroundColor = '#111';
-        }
+        var slow = function() {
+            if (light !== 'yellow') {
+                slowLight.style.backgroundColor = 'yellow';
+                stopLight.style.backgroundColor = '#111';
+                goLight.style.backgroundColor = '#111';
+            } else {
+                slowLight.style.backgroundColor = '#111';
+            }
+        };
+        slow();
     });
-    var go = document.getElementById('goButton');
-    go.addEventListener('click', function () {
+
+
+
+    var goButton = document.getElementById('goButton');
+
+
+
+    goButton.addEventListener('click', function () {
         var goLight = document.getElementById('goLight');
         var light = goLight.style.backgroundColor;
-        if (light !== 'green') {
-            goLight.style.backgroundColor = 'green';
-            slowLight.style.backgroundColor = '#111';
-            stopLight.style.backgroundColor = '#111';
-        } else {
-            goLight.style.backgroundColor = '#111';
-        }
+        function go() {
+            if (light !== 'green') {
+                goLight.style.backgroundColor = 'green';
+                slowLight.style.backgroundColor = '#111';
+                stopLight.style.backgroundColor = '#111';
+            } else {
+                goLight.style.backgroundColor = '#111';
+            }
+        };
+        go();
     });
 });
